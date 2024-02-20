db7a17171d18bdbcd7559ccee9ff437e9e8754a7
IzanBlanes <izanblanesaracil@gmail.com>
2024-02-20 21:42:11 +0100

    Creado la clase Cliente

--- src/main/java/es/cipfpbatoi/dam/psp/examen/Barberia.java
+++ src/main/java/es/cipfpbatoi/dam/psp/examen/Barberia.java
@@ -1,4 +1,5 @@
 package es.cipfpbatoi.dam.psp.examen;
 
 public class Barberia {
+
 }
--- src/main/java/es/cipfpbatoi/dam/psp/examen/Cliente.java
+++ src/main/java/es/cipfpbatoi/dam/psp/examen/Cliente.java
@@ -1,4 +1,23 @@
 package es.cipfpbatoi.dam.psp.examen;
 
+import java.util.concurrent.Semaphore;
+
 public class Cliente {
+    Semaphore semaphore = new Semaphore(1);
+
+    public static void main(String[] args) throws InterruptedException {
+
+        Cliente inicio= new Cliente();
+        inicio.inicio();
+
+    }
+    public void inicio () throws InterruptedException {
+
+        semaphore.acquire();
+        for (int i = 0; i <1000 ; i++) {
+            System.out.println("SECCION CRITICA");
+        }
+        semaphore.release();
+
+    }
 }
